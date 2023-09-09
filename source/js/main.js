104a87a04a6067331d2172fd135d715551d4c0f8
DmitryGorbi <godmni@gmail.com>
2023-09-09 08:06:48 +0300

    some changes

--- source/js/main.js
+++ source/js/main.js
@@ -19,11 +19,6 @@ if (mediaQuery.matches) {
 
 // Active main navigation link
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 35a5b162 (some changes)
 // mainNavLinks.forEach((item) => {
 //   item.addEventListener('mouseover', () => {
 //     item.classList.add('navigation__item-link--active');
@@ -40,7 +35,6 @@ const subMenu = mainNav.querySelectorAll('.navigation__submenu');
 const mainNavLinks = mainNav.querySelectorAll('.navigation__item-link span');
 // console.log(mainNavLinks.style.color = 'red')
 
-
 const subMenuButtons = mainNav.querySelectorAll('.navigation__item-button');
 
 subMenuButtons.forEach((item, index) => {
@@ -48,29 +42,16 @@ subMenuButtons.forEach((item, index) => {
     evt.preventDefault();
     subMenu[index].classList.toggle('navigation__item-link--closed');
     if (!subMenu[index].classList.contains('navigation__item-link--closed')) {
-      mainNavLinks[index].style.color = 'red'
+      mainNavLinks[index].style.color = 'red';
     }
   });
 });
 
 // окрашивает ссылку в другой цвет, когда сабменю открыто
 
-<<<<<<< HEAD
-
-
 // subMenu.forEach((item, index) => {
-  
+
 //   if (item.classList.contains('navigation__item-link--closed')) {
 //     mainNavLinks[index].style.color = 'red'
 //   }
 // });
-=======
-const mainNavLinks = document.querySelectorAll('.navigation__item-link span');
-console.log(mainNavLinks);
-
-subMenu.forEach((item, index) => {
-  if (item.classList.contains('navigation__item-link--closed')) {
-    mainNavLinks[index];
-  }
-});
->>>>>>> 35a5b162 (some changes)
