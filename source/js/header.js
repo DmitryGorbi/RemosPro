e7d30c882c60a783e242dbe6eb765db2f5113c03
DmitryGorbi <godmni@gmail.com>
2023-09-27 09:32:44 +0300

    some changes

--- source/js/header.js
+++ source/js/header.js
@@ -21,29 +21,29 @@ if (mediaQuery.matches) {
 
 // mainNavLinks.forEach((item) => {
 //   item.addEventListener('mouseover', () => {
-//     item.classList.add('navigation__item-link--active');
+//     item.classList.add('main-nav__item-link--active');
 //   });
 //   item.addEventListener('mouseleave', () => {
-//     item.classList.remove('navigation__item-link--active');
+//     item.classList.remove('main-nav__item-link--active');
 //   });
 // });
 
 // Открывает/закрывает сабменю в моильной версии и на планшете,
 // окрашивает ссылку в другой цвет, когда сабменю открыто.
 
-const subMenu = mainNav.querySelectorAll('.navigation__submenu');
-const subMenuButtonIcon = mainNav.querySelectorAll('.navigation__item-icon');
+const subMenu = mainNav.querySelectorAll('.main-nav__submenu');
+const subMenuButtonIcon = mainNav.querySelectorAll('.main-nav__item-icon');
 
-const mainNavLinks = mainNav.querySelectorAll('.navigation__item-link span');
+const mainNavLinks = mainNav.querySelectorAll('.main-nav__item-link span');
 // console.log(mainNavLinks.style.color = 'red')
 
-const subMenuButtons = mainNav.querySelectorAll('.navigation__item-button');
+const subMenuButtons = mainNav.querySelectorAll('.main-nav__item-button');
 
 subMenuButtons.forEach((item, index) => {
   item.addEventListener('click', (evt) => {
     evt.preventDefault();
-    subMenu[index].classList.toggle('navigation__item-link--closed');
-    if (!subMenu[index].classList.contains('navigation__item-link--closed')) {
+    subMenu[index].classList.toggle('main-nav__item-link--closed');
+    if (!subMenu[index].classList.contains('main-nav__item-link--closed')) {
       mainNavLinks[index].style.color = 'rgb(0, 106, 255)';
       subMenuButtonIcon[index].style.transform = 'rotate(180deg)';
     } else {
