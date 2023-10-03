50d4a8ca28b0a13a6a712208c638030ad2193084
DmitryGorbi <godmni@gmail.com>
2023-10-03 11:35:55 +0300

    changes

--- source/js/filter.js
+++ source/js/filter.js
@@ -7,13 +7,13 @@ const filterSelect = filter.querySelectorAll('.filter__select');
 
 // Универсальная функция для переключения класса активного элемента
 
-const changeActiveClass = (items, selector) => {
+const changeActiveClass = (items, disabledSelector) => {
   items.forEach((item) => {
     item.addEventListener('click', () => {
       items.forEach((el) => {
-        el.classList.remove(selector);
+        el.classList.remove(disabledSelector);
       });
-      item.classList.add(selector);
+      item.classList.add(disabledSelector);
     });
   });
 };
