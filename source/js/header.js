76b21983d54b5a167601fbb49ef11c24d247a73b
DmitryGorbi <godmni@gmail.com>
2023-10-15 18:13:06 +0300

    changes

--- source/js/header.js
+++ source/js/header.js
@@ -7,8 +7,6 @@ const onTogglerClick = (evt) => {
   evt.preventDefault();
   const isOpen = burgerMenu.getAttribute('aria-expanded') === 'false';
   burgerMenu.setAttribute('aria-expanded', isOpen);
-
-  
 };
 
 burgerMenu.addEventListener('click', onTogglerClick);
@@ -20,14 +18,11 @@ mainNav.addEventListener('keyup', (e) => {
   }
 });
 
-
 // Убираем обработчик на десктопе
 const mediaQuery = window.matchMedia('(min-width: 1240px)');
 if (mediaQuery.matches) {
   document.removeEventListener('click', onTogglerClick);
 }
-
-
 
 // Открывает/закрывает сабменю в моильной версии и на планшете,
 // окрашивает ссылку в другой цвет, когда сабменю открыто.
@@ -38,13 +33,28 @@ const subMenuButtonIcon = mainNav.querySelectorAll('.main-nav__item-icon');
 const mainNavLinks = mainNav.querySelectorAll('.main-nav__item-link span');
 // console.log(mainNavLinks.style.color = 'red')
 
-const subMenuButtons = mainNav.querySelectorAll('.main-nav__item-button');
+const subMenuButtons = mainNav.querySelectorAll('.main-nav__toggler');
+
+// subMenuButtons.forEach((item, index) => {
+//   item.addEventListener('click', (evt) => {
+//     evt.preventDefault();
+//     subMenu[index].classList.toggle('main-nav__item-link--closed');
+//     if (!subMenu[index].classList.contains('main-nav__item-link--closed')) {
+//       mainNavLinks[index].style.color = 'rgb(0, 106, 255)';
+//       subMenuButtonIcon[index].style.transform = 'rotate(180deg)';
+//     } else {
+//       mainNavLinks[index].style.color = 'inherit';
+//       subMenuButtonIcon[index].style.transform = 'rotate(0deg)';
+//     }
+//   });
+// });
 
 subMenuButtons.forEach((item, index) => {
   item.addEventListener('click', (evt) => {
     evt.preventDefault();
-    subMenu[index].classList.toggle('main-nav__item-link--closed');
-    if (!subMenu[index].classList.contains('main-nav__item-link--closed')) {
+    const isOpen = item.getAttribute('aria-expanded') === 'false';
+    item.setAttribute('aria-expanded', isOpen);
+    if (isOpen) {
       mainNavLinks[index].style.color = 'rgb(0, 106, 255)';
       subMenuButtonIcon[index].style.transform = 'rotate(180deg)';
     } else {
