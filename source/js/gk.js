ab95cae02b17edf81553612580012c836114c8ee
DmitryGorbi <godmni@gmail.com>
2024-04-04 06:35:33 +0300

    changes

--- source/js/gk.js
+++ source/js/gk.js
@@ -6,21 +6,21 @@ let mapTagPopups = document.querySelectorAll('.mapTag__offer');
 
 ableLayers.forEach((item, index) => {
   item.addEventListener('mouseover', () => {
-    mapTagPopups[index].classList.add('mapTag__offer--show')
-  })
+    mapTagPopups[index].classList.add('mapTag__offer--show');
+  });
   item.addEventListener('mouseout', () => {
-    mapTagPopups[index].classList.remove('mapTag__offer--show')
-  })
-})
+    mapTagPopups[index].classList.remove('mapTag__offer--show');
+  });
+});
 
-const tableToggle = document.querySelector('.gk-offers__item button');
-const gkTable = document.querySelector('.gk-table');
+const tableToggle = document.querySelector('.gkOffers__item button');
+const gkTable = document.querySelector('.gkTable');
 const gkTableBody = gkTable.querySelector('tbody');
 const gkTableFooter = gkTable.querySelector('tfoot');
-console.log(tableToggle)
+console.log(tableToggle);
 
 tableToggle.addEventListener('click', () => {
   gkTable.classList.toggle('visually-hidden');
   gkTableBody.classList.toggle('visually-hidden');
   gkTableFooter.classList.toggle('visually-hidden');
-})
+});
